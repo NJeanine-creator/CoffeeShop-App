3c1af6a3ba2d4e636f5f234bb719a6ed3558d61b
NJeanine-creator <jnishimwe321@daviscollege.com>
2021-05-03 18:32:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/coffeeshop/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.coffeeshop.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.coffeeshop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
